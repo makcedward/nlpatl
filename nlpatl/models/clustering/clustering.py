e6dacd45de4e784542f2bf591abd6650b2985b53
Edward Ma <makcedward@gmail.com>
2021-11-27 10:21:52 -0800

    revamp sci-kit learn cluster wrapper

--- nlpatl/models/clustering/clustering.py
+++ nlpatl/models/clustering/clustering.py
@@ -5,5 +5,11 @@ class Clustering:
 	def __init__(self, name: str):
 		self.name = name
 
+	def train(self):
+		...
+
+	def predict(self):
+		...
+
 	def convert(self, inputs: List[str]) -> List[float]:
 		...
--- nlpatl/models/clustering/sklearn.py
+++ nlpatl/models/clustering/sklearn.py
@@ -1,19 +1,56 @@
-from __future__ import annotations
 from typing import List
+from collections import defaultdict
+import numpy as np
+from sklearn.cluster import (
+	KMeans
+)
 
 from nlpatl.models.clustering.clustering import Clustering
+from nlpatl.storage.storage import Storage
+
+MODEL_FOR_SKLEARN_CLUSTERING_MAPPING_NAMES = {
+    'kmeans': KMeans,
+}
 
 
 class SkLearn(Clustering):
-    def __init__(self, name: str = 'sklearn'):
-        super().__init__(name=name)
+	def __init__(self, model_name: str = 'kmeans', model_config: dict = {}, 
+		name: str = 'kmeans'):
 
-    def fit(self, inputs: List[float]) -> SkLearn:
-        self.model.fit(inputs)
-        return self
+		super().__init__(name)
 
-    def cluster(self, inputs: List[float], num_nearest: int = 5) -> List[int]:
-        ...
+		self.model_name = model_name
+		self.model_config = model_config
 
-    def save(self) -> None:
-        ...
+		if model_name in MODEL_FOR_SKLEARN_CLUSTERING_MAPPING_NAMES:
+			self.model = MODEL_FOR_SKLEARN_CLUSTERING_MAPPING_NAMES[model_name](
+				**model_config)
+		else:
+			raise ValueError('`{}` does not support. Supporting {} only'.format(
+				model_name, '`' + '`'.join(
+					MODEL_FOR_SKLEARN_CLUSTERING_MAPPING_NAMES.keys()) + '`'))
+
+	@staticmethod
+	def get_mapping() -> dict:
+		return MODEL_FOR_SKLEARN_CLUSTERING_MAPPING_NAMES
+
+	def train(self, inputs: List[float]):
+		self.model.fit(inputs)
+
+	def predict_prob(self, inputs: List[float], 
+		predict_config: dict={}) -> List[Storage]:
+
+		num_cluster = self.model.n_clusters
+
+		clust_dists = self.model.transform(inputs)
+		preds = self.model.predict(inputs, **predict_config)
+
+		results = defaultdict(Storage)
+		for label in range(self.model.n_clusters):
+			indices = np.where(preds == label)[0]
+
+			results[label] = Storage(
+				indices=indices, group=label, 
+				values=clust_dists[indices][:, label])
+
+		return results
