4fb727b69ea6cff7943a716112bc9f934304e829
Edward Ma <makcedward@gmail.com>
2021-11-28 08:05:40 -0800

    add module shortcut

---
+++ nlpatl/models/__init__.py
@@ -0,0 +1,4 @@
+from nlpatl.models.learning.uncertainty.entropy import EntropySampling
+from nlpatl.models.learning.uncertainty.margin import MarginSampling
+
+from nlpatl.models.learning.unsupervised.clustering import ClusteringSamlping
