4fc35153ddd4fa13412cce8472be412b44c10386
Edward Ma <makcedward@gmail.com>
2021-11-29 17:05:11 -0800

    revamp UnsupervisedLearning

--- nlpatl/models/learning/unsupervised/clustering.py
+++ nlpatl/models/learning/unsupervised/clustering.py
@@ -2,22 +2,26 @@ from typing import List
 from collections import defaultdict
 import numpy as np
 
-from nlpatl.models.learning.learning import Learning
+from nlpatl.models.clustering.clustering import Clustering
+from nlpatl.models.embeddings.embeddings import Embeddings
+from nlpatl.models.learning.unsupervised_learning import UnsupervisedLearning
 from nlpatl.storage.storage import Storage
 
 
-class ClusteringSamlping(Learning):
-	def __init__(self, name: str = 'clustering_samlping'):
+class ClusteringLearning(UnsupervisedLearning):
+	def __init__(self, multi_label: bool = False, 
+		embeddings_model: Embeddings = None, 
+		clustering_model: Clustering = None, 
+		name: str = 'clustering_learning'):
 
-		super().__init__(name=name)
-
-	def validate(self):
-		super().validate(['embeddings', 'clustering'])
+		super().__init__(multi_label=multi_label, 
+			embeddings_model=embeddings_model,
+			clustering_model=clustering_model,
+			name=name)
 
 	def keep_most_valuable(self, data: Storage, 
 		num_sample: int) -> Storage:
 	
-		# TODO: Tuning me. Allocating `to_be_filter_indices` size first
 		to_be_filter_indices = []
 		for group in np.unique(data.groups):
 			indices = np.where(data.groups == group)[0]
@@ -32,20 +36,3 @@ class ClusteringSamlping(Learning):
 		data.keep(np.concatenate(to_be_filter_indices))
 
 		return data
-
-	def explore(self, inputs: List[str], return_type: str = 'dict', 
-		num_sample: int = 2) -> [Storage, dict]:
-
-		self.validate()
-
-		features = self.embeddings_model.convert(inputs)
-
-		self.clustering_model.train(features)
-		preds = self.clustering_model.predict_prob(features)
-
-		preds = self.keep_most_valuable(preds, num_sample=num_sample)
-
-		preds.features = [inputs[i] for i in preds.indices.tolist()]
-
-		return self.get_return_object(preds, return_type)
-		
--- tests/models/learning/unsupervised/test_clustering.py
+++ tests/models/learning/unsupervised/test_clustering.py
@@ -1,35 +1,24 @@
+from typing import List
+from datasets import load_dataset
 import unittest
 import datasets
-from datasets import load_dataset
+import numpy as np
 
-from nlpatl.models import ClusteringSamlping
+from nlpatl.models.embeddings import Embeddings
+from nlpatl.models.clustering.clustering import Clustering
+from nlpatl.models.learning.unsupervised.clustering import ClusteringLearning
+from nlpatl.storage.storage import Storage
 
 
-class TestModelLearningClusteringSampling(unittest.TestCase):
+class TestModelLearningClustering(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
 		cls.train_texts = load_dataset('ag_news')['train']['text'][:20]
 
-	def test_no_model(self):
-		learning = ClusteringSamlping()
-
-		with self.assertRaises(Exception) as error:
-			learning.explore(self.train_texts)
-		assert 'Embeddings model does not initialize yet' in str(error.exception), \
-			'Does not initialize embeddings model but still able to run'
-
-		learning.init_embeddings_model(
-			'distilbert-base-uncased', return_tensors='pt', padding=True, 
-			batch_size=3)
-		with self.assertRaises(Exception) as error:
-			learning.explore(self.train_texts)
-		assert 'Clustering model does not initialize yet' in str(error.exception), \
-			'Does not initialize clustering model but still able to run'
-
 	def test_explore(self):
-		learning = ClusteringSamlping()
+		learning = ClusteringLearning()
 		learning.init_embeddings_model(
-			'bert-base-uncased', return_tensors='pt', padding=True, 
+			'bert-base-uncased', return_tensors='pt', padding=True,
 			batch_size=3)
 		learning.init_clustering_model(
 			'kmeans', model_config={})
