1d1ae89ad3225fbbbf9654faeb2190f8220c3c8e
Edward Ma <makcedward@gmail.com>
2021-12-01 21:58:45 -0800

    support image input

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ _deps = [
     'xgboost>=1.5.1',
     'datasets>=1.8.0',
     'torch>=1.9.0',
-    'tensorflow>=2.7.0'
+    'tensorflow>=2.7.0',
+    'Pillow>=8.2.0'
 ]
 
 # support both w and w/ version
@@ -29,7 +30,11 @@ def deps_list(*pkgs):
 install_reqs = deps_list('scipy', 'numpy', 'scikit-learn', 'transformers')
 
 extra_reqs = {}
-extra_reqs['dev'] = deps_list('datasets', 'xgboost', 'torch', 'tensorflow')
+extra_reqs['image'] = deps_list('Pillow')
+extra_reqs['dev'] = (
+    deps_list('datasets', 'xgboost', 'torch', 'tensorflow')
+    + extra_reqs['image']
+)
 
 extra_reqs['all'] = (
     install_reqs
